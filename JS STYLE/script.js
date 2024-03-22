99fc734d832fc70c6ce809110ab7c607501bd2f2
Sereoja20 <159931910+Sereoja20@users.noreply.github.com>
2024-03-22 12:25:01 +0300

    first file

---
+++ JS STYLE/script.js
@@ -0,0 +1,31 @@
+get = document.getElementById("rgb");
+function redButtononclick(){ 
+    get.style.color = 'red';
+}
+function orangeButtononclick(){
+    get.style.color = 'orange';
+    
+}
+function YELLOWButtononclick(){
+    get.style.color = 'yellow';
+    
+}
+function GREENButtononclick(){
+    get.style.color = 'green';
+    
+}
+function LIGHTBLUEButtononclick(){
+    get.style.color = 'lightblue';
+    
+}
+function BLUEButtononclick(){
+    get.style.color = 'blue';
+    
+}
+function VIOLETButtononclick(){
+    get.style.color = 'violet';
+    
+}
+function BLACKButtononclick(){
+    get.style.color = 'BLACK';
+}
